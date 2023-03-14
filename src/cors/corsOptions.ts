3c17ab4b7c83954d07d2c522513f253d3e653231
JoshAlexis <joshyelc@gmail.com>
2023-03-14 09:04:04 -0600

    fix(deps): add @types/cors and params types

--- src/cors/corsOptions.ts
+++ src/cors/corsOptions.ts
@@ -6,7 +6,8 @@ export const allowedOrigins = process.env.CORS_ORIGINS?.split(',')
  * Define las opciones para CORS.
  */
 export const corsOptions: CorsOptions = {
-	origin: (origin, callback) => {
+	// eslint-disable-next-line no-shadow
+	origin: (origin: string | undefined, callback: (err: Error | null, origin?: any | undefined) => void) => {
 		if (!origin) return callback(null, true)
 		if (allowedOrigins?.includes(origin)) return callback(null, true)
 		return callback(new Error('No allowed by CORS'))
