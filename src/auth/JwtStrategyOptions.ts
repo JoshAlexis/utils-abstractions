fc779b40245a188373b411863c9cb2cea1821621
JoshAlexis <joshyelc@gmail.com>
2024-02-14 12:50:30 -0600

    fix(types): change type assertion for `algorithms`

--- src/auth/JwtStrategyOptions.ts
+++ src/auth/JwtStrategyOptions.ts
@@ -1,4 +1,5 @@
 import { ExtractJwt, StrategyOptions } from 'passport-jwt'
+import { Algorithm } from 'jsonwebtoken'
 
 /**
  * Default configurations for `passport-jwt` with:
@@ -11,7 +12,7 @@ import { ExtractJwt, StrategyOptions } from 'passport-jwt'
  */
 export const JWT_STRATEGY_OPTIONS: StrategyOptions = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-	secretOrKey: process.env?.JWT_ACCESS_SECRET,
+	secretOrKey: process.env?.JWT_ACCESS_SECRET as string,
 	issuer: process.env?.JWT_ISSUER,
-	algorithms: [process.env?.JWT_ALGORITHM as string]
+	algorithms: [process.env?.JWT_ALGORITHM as Algorithm]
 }
