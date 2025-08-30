dc9cfaa52d7c70bf7f845f1cc416144eeade6e6e
JoshAlexis <joshyelc@gmail.com>
2025-08-29 22:53:33 -0700

    test(config): add support for dynamic field/cookie masking in tests
    - Add `.env.test` file to configure fields and
    cookies to mask.
    - Update `hideField.spec.ts` with tests for
    ENV-configured fields and cookies.
    - Install `dotenv` for environment variable
    management in tests.

--- SetupTests.ts
+++ SetupTests.ts
@@ -1,1 +1,6 @@
 import 'reflect-metadata'
+import 'dotenv/config'
+import * as path from 'path'
+
+// Sobrescribe el archivo de entorno por defecto
+require('dotenv').config({ path: path.resolve(__dirname, './.env.test') })
--- tests/middlewares/hideField.spec.ts
+++ tests/middlewares/hideField.spec.ts
@@ -31,4 +31,30 @@ describe('hideField', () => {
 		expect(result.cookie).toBeDefined()
 		expect((result.cookie as Record<string, string>).jwt).toBe('********************')
 	})
+
+	it('Should hide value of header from ENV', () => {
+		const data = {
+			authorization: 'Bearer token',
+			'x-custom-header': 'custom-value'
+		}
+
+		const result = hideField(data)
+
+		expect(result.authorization).toBe('********************')
+		expect(result['x-custom-header']).toBe('********************')
+	})
+
+	it('Should hide value of cookie from ENV', () => {
+		const data = {
+			cookie: {
+				jwt: 'token',
+				'session-id': 'custom-value'
+			}
+		}
+
+		const result = hideField(data)
+
+		expect((result.cookie as Record<string, string>).jwt).toBe('********************')
+		expect((result.cookie as Record<string, string>)['session-id']).toBe('********************')
+	})
 })
